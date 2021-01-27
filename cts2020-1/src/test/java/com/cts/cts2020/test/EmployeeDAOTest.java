4a4f7f972dafca19407014a9f21b135581ad52e1
Gyanendra Singh <gyanendra.singh@congnizant.com>
2021-01-27 17:09:52 +0530

    added Mockito

--- cts2020-1/src/test/java/com/cts/cts2020/test/EmployeeDAOTest.java
+++ cts2020-1/src/test/java/com/cts/cts2020/test/EmployeeDAOTest.java
@@ -3,10 +3,12 @@ package com.cts.cts2020.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,6 +19,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.cts.cts2020.Employee;
 import com.cts.cts2020.EmployeeMapper;
 import com.cts.cts2020.dao.EmployeeDAO;
+import static org.mockito.Mockito.*;
 
 class EmployeeDAOTest {
 
@@ -25,11 +28,18 @@ class EmployeeDAOTest {
 		Resource resource=new ClassPathResource("applicationContext.xml");  
 	    BeanFactory factory=new XmlBeanFactory(resource);  
 	    EmployeeDAO empdao=(EmployeeDAO)factory.getBean("daoOfEmp");  
+	    
+	    EmployeeDAO empdao1 = Mockito.mock(EmployeeDAO.class);
+	    List<Employee> l1 = new ArrayList();
+	    l1.add(new Employee());
+ 	    Mockito.when(empdao1.getAllEmployees()).thenReturn(l1);
 		
 		// EmployeeDAO empdao = new EmployeeDAO(); 
-		List<Employee> l = empdao.getAllEmployees();
+		List<Employee> l = empdao1.getAllEmployees();
 		Assertions.assertEquals(116, l.size());
 	}
+	
+	// create void testGetAllDepartmentsMock() - just use mock.
 	
 	@Test
 	void testGetConnection() {
@@ -58,10 +68,8 @@ class EmployeeDAOTest {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
 		JdbcTemplate jdb = (JdbcTemplate) context.getBean("jdbcTemplate");
-		
-		
-		
-		String sql  ="select * from employees";
+	
+		String sql  ="select * from employees"; //where first_name = "Steven"
 		List<Employee> l = jdb.query(
 				   sql, new EmployeeMapper());
 		System.out.println(l.get(0).getName());
